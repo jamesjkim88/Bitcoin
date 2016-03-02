4d11db2be905a18503ad4886990480568498e4bc
James Kim <jamesjkim8806@gmail.com>
2016-03-01 23:37:47 -0800

    different labels

--- public/app/app.js
+++ public/app/app.js
@@ -1,11 +1,15 @@
 var app = angular.module("bitcoinApp", []);
 
 app.controller("mainCtrl", function($scope, $http){
-    $scope.currencies = [{id: 1, label: 'aud'}, {id: 2, label: 'eur'}, {id:3, label: 'usd'}];
+    $scope.currencies = [
+        {id: 1, label: 'Australian', code: "aud"},
+        {id: 2, label: 'Euro', code: "eur"},
+        {id:3, label: 'US Dollar', code: "usd"}
+    ];
 
     $scope.click = function(currency){
         console.log($scope.selected);
-        $http.get("/bitcoin?currency=" + $scope.selected.label).then(function(config){
+        $http.get("/bitcoin?currency=" + $scope.selected.code).then(function(config){
             console.log("data: ", config);
             $scope.data = JSON.parse(config.data.body);
         }, function(config){

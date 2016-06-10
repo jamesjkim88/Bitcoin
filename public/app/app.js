7cb3a4779deae2c6b0d2ff5d893c12cb8c6e7bad
James Kim <jamesjkim8806@gmail.com>
2016-06-10 12:26:17 -0700

    minor bugs fixed push to heroku

--- public/app/app.js
+++ public/app/app.js
@@ -2,26 +2,27 @@ var app = angular.module("bitcoinApp", []);
 
 app.controller("mainCtrl", function($scope, $http){
     $scope.currencies = [
-        {id: 1, label: 'Australian Dollar', code: "aud"},
-        {id: 2, label: 'British Pound', code: "gbp"},
-        {id: 3, label: 'Brazilian Real', code: "brl"},
-        {id: 4, label: 'Canadian Dollar', code: "cad"},
-        {id: 5, label: 'Euro', code: "eur"},
-        {id: 6, label: 'Japanese Yen', code: "jpy"},
-        {id: 7, label: 'Singapore Dollar', code: "sgd"},
-        {id: 8, label: 'South Korean Won', code: "krw"},
-        {id: 9, label: 'Swiss Franc', code: "chf"},
-        {id: 10, label: 'Thai Baht', code: "thb"},
-        {id: 11, label: 'US Dollar', code: "usd"}
+        {id: 1, label: 'Australian Dollar', code: "AUD"},
+        {id: 2, label: 'British Pound', code: "GBP"},
+        {id: 3, label: 'Brazilian Real', code: "BRL"},
+        {id: 4, label: 'Canadian Dollar', code: "CAD"},
+        {id: 5, label: 'Euro', code: "EUR"},
+        {id: 6, label: 'Japanese Yen', code: "JPY"},
+        {id: 7, label: 'Singapore Dollar', code: "SGD"},
+        {id: 8, label: 'South Korean Won', code: "KRW"},
+        {id: 9, label: 'Swiss Franc', code: "CHF"},
+        {id: 10, label: 'Thai Baht', code: "THB"},
+        {id: 11, label: 'US Dollar', code: "USD"}
     ];
 
     $scope.click = function(currency){
         $http.get("/bitcoin?currency=" + $scope.selected.code).then(function(config){
+            console.log("config: ", config);
             $scope.data = JSON.parse(config.data.body);
             $scope.total = $scope.data.amount * $scope.amount;
             // $(".amount").addClass("show");
             var text = '<p class="show amount animated zoomIn">' + $scope.total.toFixed(2) + " " + $scope.data.currency + '</p>';
-            $("#totalBox").append(text)
+            $("#totalBox").html(text)
         }, function(config){
             console.log("configError: ", config);
         });
